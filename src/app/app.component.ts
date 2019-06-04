74eaf5f78e1244512d2648a6114971e0d15b4a50
kumarshan <kumaruidivloper@gmail.com>
2019-06-04 13:02:12 +0530

    ng-ant-layout-type-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-ant-ui';
 
-  gutter = 16;
-  count = 4;
-  marksGutter = {
-    8: 8,
-    16: 16,
-    24: 24,
-    32: 32,
-    40: 40,
-    48: 48
-  };
-  marksCount = {
-    2: 2,
-    3: 3,
-    4: 4,
-    6: 6,
-    8: 8,
-    12: 12
-  };
-
-  generateArray(value: number): number[] {
-    return new Array(value);
-  }
+  isCollapsed = false;
+  isReverseArrow = false;
+  width = 200;
 }
